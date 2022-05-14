6cca87cb732579e902aa1a1a0ded6c4446edef28
lifeNewbi <lastchallenged@gmail.com>
2022-05-15 01:00:39 +0900

    Update Object

--- app/src/main/java/com/example/naver_map_test/Send_request.java
+++ app/src/main/java/com/example/naver_map_test/Send_request.java
@@ -3,11 +3,48 @@ package com.example.naver_map_test;
 import java.util.HashMap;
 
 public class Send_request {
+
+    // 현재 위도 경도
     double latitude;
     double longitude;
+    // conv, meal, cafe
+    String category;
+    // 통신사
+    String carrier;
+    // 등급
+    String rate;
+    private Object send_request;
 
-    public Send_request(double latitude, double longitude) {
+
+
+    public Send_request(double latitude, double longitude, String category, String carrier, String rate) {
         this.latitude = latitude;
         this.longitude = longitude;
+
+        this.category = category;
+        this.carrier = carrier;
+        this.rate = rate;
+    }
+
+}
+
+class body_Object {
+    // 현재 위도 경도
+    double latitude;
+    double longitude;
+    // conv, meal, cafe
+    String category;
+    // 통신사
+    String carrier;
+    // 등급
+    String rate;
+
+    body_Object(double latitude, double longitude, String category, String carrier, String rate) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.category = category;
+        this.carrier = carrier;
+        this.rate = rate;
     }
 }
+
